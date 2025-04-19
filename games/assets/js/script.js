d3dd5eea578d9d6b972b9207d17627d6ec0dc15e
rEHEPATOPyPOHA <149316102+rEHEPATOPyPOHA@users.noreply.github.com>
2025-04-19 14:53:41 +0300

    Delete games directory

--- games/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// includes - проверка наличия
-function checkEmail() {
-    let email = document.querySelector('#emailField').value;
-    if (!email.includes('@')) alert('Where is @');
-    else if (!email.includes('.')) alert('Where is DOT');
-    else alert ('Good news. Alright!')
-}
